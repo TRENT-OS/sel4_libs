2668cc89e7ff388964e64f79060d7658ea251f5b
Sebastian Eckl <sebastian.eckl@hensoldt.net>
2022-08-02 13:37:20 +0200

    [DEV] add support for Jetson Xavier NX Developer Kit

---
+++ libsel4bench/arch_include/arm/cpu/nvidia-carmel/sel4bench/cpu/events.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022, HENSOLDT Cyber GmbH
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#pragma once
+#define SEL4BENCH_EVENT_BUS_ACCESS_LD         0x60
+#define SEL4BENCH_EVENT_BUS_ACCESS_ST         0x61
+#define SEL4BENCH_EVENT_BR_INDIRECT_SPEC      0x7A
+#define SEL4BENCH_EVENT_EXC_IRQ               0x86
+#define SEL4BENCH_EVENT_EXC_FIQ               0x87
